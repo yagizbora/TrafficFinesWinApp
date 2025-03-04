fed268e7555d40b0c8b1ae73042c3c6f3b8792b7
Yağız Bora <yaizbora@gmail.com>
2025-03-04 03:57:51 +0300

    Created project and setup env file

---
+++ TrafficFines/Form1.cs
@@ -0,0 +1,39 @@
+﻿using DotNetEnv;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+
+namespace TrafficFines
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection? connection;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                Env.Load();
+                string connectionString = Env.GetString("DB_CONNECTION_STRING");
+
+                if (string.IsNullOrEmpty(connectionString))
+                {
+                    Console.WriteLine(connectionString);
+                    MessageBox.Show("Bağlantı dizesi null veya boş. Lütfen .env dosyasını kontrol edin.");
+                    return;
+                }
+
+                connection = new SqlConnection(connectionString);
+                Console.WriteLine(connectionString);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata: " + "\n" + (ex?.Message ?? "Bilinmeyen bir hata oluştu."));
+            }
+        }
+    }
+}
